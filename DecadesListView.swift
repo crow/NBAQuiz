031e1fd5fe379e8ed8b13bbaf259677f18c9a642
dcrow <jdavidcrow@gmail.com>
2020-03-24 11:25:08 -0700

    add list to project and fix list

--- DecadesListView.swift
+++ DecadesListView.swift
@@ -15,7 +15,7 @@ struct DecadeRow : View {
 
 struct DecadesListView: View {
     @State private var showingAlert = false
-    var teams:[Decade] = [
+    var decades:[Decade] = [
         Decade(name:"1960", color:Color.blue),
         Decade(name:"1970", color:Color.red),
         Decade(name:"1980", color:Color.green),
@@ -26,17 +26,18 @@ struct DecadesListView: View {
             ]
     var body: some View {
         List{
-            Button(action: {
-                self.showingAlert = true
-            }) {
-            }
-            ForEach(teams, id: \.name) {team in
-                DecadeRow(title: team.name, color: team.color)
-            }
-            
-            
-            .alert(isPresented: $showingAlert) {
-                Alert(title: Text("Are you sure you want to choose", Decade, "?"), message: Text("Time to start", Decade, "quiz"), dismissButton: .default(Text("Choose another decade and try again")))
+
+            ForEach(decades, id: \.self) {decade in
+                ZStack {
+                    Button(action: {
+                        self.showingAlert = true
+                    }) {
+                        Text("")
+                    }.alert(isPresented: self.$showingAlert) {
+                        Alert(title: Text("Are you sure you want to choose \(decade.name)?"), message: Text("Time to start \(decade.name) quiz"), dismissButton: .default(Text("Choose another decade and try again")))
+                    }
+                    DecadeRow(title: decade.name, color: decade.color)
+                }
             }
         }
     }
@@ -48,7 +49,7 @@ struct DecadesListView_Previews: PreviewProvider {
     }
 }
 
-struct Decade {
+struct Decade: Hashable {
     let name:String
     let color:Color
 }
