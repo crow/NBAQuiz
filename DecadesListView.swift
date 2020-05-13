af8fcc35cc77628cafaf894bd64007b63a006ad0
dcrow <jdavidcrow@gmail.com>
2020-05-13 14:06:58 -0700

    added 2020 quiz transition

--- DecadesListView.swift
+++ DecadesListView.swift
@@ -15,6 +15,8 @@ struct DecadeRow : View {
 
 struct DecadesListView: View {
     @State private var showingAlert = false
+    @State private var showingQuiz = false
+
     var decades:[Decade] = [
         Decade(name:"1960", color:Color.blue),
         Decade(name:"1970", color:Color.red),
@@ -23,23 +25,32 @@ struct DecadesListView: View {
         Decade(name:"2000", color:Color.orange),
         Decade(name:"2010", color:Color.purple),
         Decade(name:"2020", color:Color.red)
-            ]
+    ]
     var body: some View {
-        List{
-            ForEach(decades, id: \.self) {decade in
-                ZStack {
-                    Button(action: {
-                        self.showingAlert = true
-                    }) {
-                        Text("")
-                    }.alert(isPresented: self.$showingAlert) {
-                        Alert(title: <#T##Text#>, message: <#T##Text?#>, primaryButton: Button(action: {
-                            <#code#>
-                        }, label: <#T##() -> _#>), secondaryButton: <#T##Alert.Button#>)
+        ZStack {
+            NavigationLink(destination: Text("Make me into a quiz view that displays sheet alerts for each question"), isActive: self.$showingQuiz) { EmptyView() }
+            List{
+                ForEach(decades, id: \.self) {decade in
+                    ZStack {
+                        Button(action: {
+                            self.showingAlert = true
+                        }) {
+                            Text("")
+                        }
+                        .alert(isPresented: self.$showingAlert) {
+                            Alert(title: Text("Are you sure you want to choose \(decade.name)?"), message: Text("Time to start \(decade.name) quiz"), primaryButton: .default(Text("Start \(decade.name) quiz"), action: {
+
+                                self.showingQuiz.toggle()
+                            }), secondaryButton: .default(Text("Cancel"), action: {
+                                self.showingQuiz.toggle()
+                            }))
+                            //                        return Alert(title: Text("Are you sure you want to choose \(decade.name)?"), message: Text("Time to start \(decade.name) quiz"), dismissButton: .default(Text("Choose another decade and try again")))
+                        }
+                        DecadeRow(title: decade.name, color: decade.color)
                     }
-                    DecadeRow(title: decade.name, color: decade.color)
                 }
             }
+
         }
     }
 }
